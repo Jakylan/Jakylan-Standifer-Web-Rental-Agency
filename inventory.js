d4b24d56c2e72b14980d97069ae227cd7a695b5d
Jakylan <jstandifer@basecampcodingacademy.org>
2018-10-18 09:56:40 -0500

    my pics

--- inventory.js
+++ inventory.js
@@ -1,32 +1,37 @@
 const INVENTORY = {
-    Yeti: {
-        name: "Yeti",
-        price: 6500,
-        In_Stock: 1
-    },
-    Trek: {
-        name: "Trek",
-        price: 3500,
-        In_Stock: 1
-    },
-    Specialized: {
-        name: "Specialized",
-        price: 3500,
-        In_Stock: 1
-    },
-    Santa_Cruz: {
-        name: "Santa Cruz",
-        price: 5500,
-        In_Stock: 1
-    },
-    Merida: {
-        name: "Merida",
-        price: 4500,
-        In_Stock: 1
-    },
-    GT: {
-        name: "GT",
-        price: 5500,
-        In_Stock: 1
+    inventory: {
+        bikes: [
+            (Yeti: {
+                name: "Yeti",
+                coverImage: /home/basecamp/web_rental/yeti.jpg,
+                price: 6500,
+                In_Stock: 1
+            }),
+            (Trek: {
+                name: "Trek",
+                price: 3500,
+                In_Stock: 1
+            }),
+            (Specialized: {
+                name: "Specialized",
+                price: 3500,
+                In_Stock: 1
+            }),
+            (Santa_Cruz: {
+                name: "Santa Cruz",
+                price: 5500,
+                In_Stock: 1
+            }),
+            (Merida: {
+                name: "Merida",
+                price: 4500,
+                In_Stock: 1
+            }),
+            (GT: {
+                name: "GT",
+                price: 5500,
+                In_Stock: 1
+            })
+        ]
     }
 };
