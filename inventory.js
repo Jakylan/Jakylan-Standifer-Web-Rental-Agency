184c9adb7d4ef352084d2f1e1b160338d61cd25b
Jakylan <jstandifer@basecampcodingacademy.org>
2018-10-19 10:09:04 -0500

    getting my pics to show up

--- inventory.js
+++ inventory.js
@@ -1,37 +1,37 @@
 const INVENTORY = {
     bikes: [
         {
-            image: "/home/basecamp/web_rental/yeti.jpg",
+            image: "yeti.jpg",
             name: "Yeti",
             price: 65,
             in_stock: 1
         },
         {
-            image: "/home/basecamp/web_rental/trek.jpg",
+            image: "trek.jpg",
             name: "Trek",
             price: 40,
             in_stock: 1
         },
         {
-            image: "/home/basecamp/web_rental/specialized.jpg",
+            image: "specialized.jpg",
             name: "Specialized",
             price: 3500,
             in_stock: 1
         },
         {
-            image: "/home/basecamp/web_rental/santacruz.jpg",
+            image: "santacruz.jpg",
             name: "Santa Cruz",
             price: 55,
             in_stock: 1
         },
         {
-            image: "/home/basecamp/web_rental/merida.jpg",
+            image: "merida.jpg",
             name: "Merida",
             price: 45,
             in_stock: 1
         },
         {
-            image: "/home/basecamp/web_rental/gt.jpg",
+            image: "gt.jpg",
             name: "GT",
             price: 55,
             in_stock: 1
